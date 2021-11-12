c51163bffe63f22f070f6505d89c0e47b96c2af3
Joshua Salinas <joshuasalinashernandez@gmail.com>
2021-11-12 14:49:35 -0600

    Adding new updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,69 @@
-const name = 'Joshua';
+/*const name = 'Joshua';
 const  age = 23;
 
 const s = 'Hello world, to, my, job';
 
 console.log(s.toUpperCase());
 console.log(s.split(', '));
-console.log('Hello, my name is '+name+' and my age is'+ age);
+console.log('Hello, my name is '+name+' and my age is'+ age);*/
+
+//Arrays
+
+const numbers = new Array(1,2,3,4,5,6);
+
+const fruits = ['orange','pears',10,true]
+
+fruits[3] = 'grapes';
+fruits.push('mangos');
+fruits.unshift('fruit at firts');
+fruits.pop(); //delete the last one
+console.log(Array.isArray(fruits)); //testing about array
+console.log(fruits);
+
+//building an object
+const person = {
+    first_name: 'Joshua',
+    last_name: 'Salinas',
+    age: 23,
+    hobbies: ['music','games'],
+    adress:{
+        street: 'unknow',
+        city: 'far far away',
+        state: 'Mx'
+    }
+}
+//acceding to an array and object
+console.log(person.hobbies[0],person.adress.state);
+
+const everyone = [
+    {
+        id: 1,
+        text: 'one text',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'second text',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        text: 'three text',
+        isCompleted: true
+    }
+];
+
+console.log(everyone[1].text);
+
+//parsing this to JSON
+const everyoneJSON = JSON.stringify(everyone);
+console.log(everyoneJSON);
+
+for(let every of everyone){
+    console.log(every);
+}
+//or same for but old way
+for (let i=0; i<everyone.length; i++){
+    console.log(everyone);
+}
+
