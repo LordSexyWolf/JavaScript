467ae8a57f115c09008135380da1fbf323866068
Joshua Salinas <joshuasalinashernandez@gmail.com>
2021-11-12 12:27:18 -0600

    consoleLog

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const name = 'Joshua';
+const  age = 23;
+
+const s = 'Hello world, to, my, job';
+
+console.log(s.toUpperCase());
+console.log(s.split(', '));
+console.log('Hello, my name is '+name+' and my age is'+ age);
